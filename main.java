f11c96b53d111d150e95b800a35771b601edbd19
Alejandro Kincaid <akincaidrivade0215@myduneland.org>
2021-11-08 08:54:05 -0600

    u

--- main.java
+++ main.java
@@ -7,27 +7,10 @@
  */
 public class main
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class main
-     */
-    public main()
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        double value = Review.sentimentVal("badly");    
+        System.out.println(value);
     }
 }
+
